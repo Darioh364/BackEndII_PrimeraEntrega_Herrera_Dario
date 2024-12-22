8647e43568e545bd80856de063f3f0c96f12626c
Dario Herrera <herreradarioeliezer@gmail.com>
2024-12-22 16:55:35 -0300

    Configuracion del proyecto final

--- src/routes/carritos.routes.js
+++ src/routes/carritos.routes.js
@@ -1,65 +1,14 @@
 import { Router } from "express";
-import crypto from 'crypto'
-import {__dirname} from '../path.js'
-import {promises as fs} from 'fs';
-import path from 'path';
+import { getCart, createCart, insertProductCart, updateProductsCart, updateQuantityProductCart, deleteCart, deleteProductCart} from "../controllers/carts.controllers.js";
 
 const cartRouter = Router()
 
-const carritosPath = path.resolve(__dirname, '../src/db/carritos.json'); //Dada una ruta, las concateno entre si (evito utiliza el +)
-
-// Leer el archivo
-const carritosData = await fs.readFile(carritosPath, 'utf-8');
-const carritos = JSON.parse(carritosData);
-
-//Consultar los productos guardados en un carrito
-cartRouter.get('/:cid', (req,res)=> {
-    const idCarrito = req.params.cid
-    const carrito = carritos.find(cart => cart.id == idCarrito)
-
-    if(carrito) {
-        console.log(carrito);
-        
-        res.status(200).send(carrito.products)
-    } else {
-        res.status(404).send({mensaje: "El carrito no existe"})
-    }
-})
-
-//Crear un nuevo carrito
-cartRouter.post('/', async (req,res) => {
-    const newCart = {
-        id: crypto.randomBytes(5).toString('hex'),
-        products: []
-    }
-    carritos.push(newCart)
-    await fs.writeFile(carritosPath, JSON.stringify(carritos))
-    res.status(200).send(`Carrito creado correctamente con el id ${newCart.id}`)
-})
-
-//Agregar nuevo producto al carrito
-cartRouter.post('/:cid/products/:pid', async (req,res) => {
-    const idCarrito = req.params.cid
-    const idProducto = req.params.pid
-    const {quantity} = req.body
-
-    const carrito = carritos.find(cart => cart.id == idCarrito)
-
-    if(carrito) {
-        const indice = carrito.products.findIndex(prod => prod.id == idProducto)
-        
-        if(indice != -1) { //Si el producto existe, piso con la nueva cantidad
-            let quantityActual =  carrito.products[indice].quantity
-            let quantityNueva = quantityActual + quantity
-            carrito.products[indice].quantity = quantityNueva
-        } else { //Si el producto no existe, lo creo y lo guardo
-            carrito.products.push({id: idProducto, quantity: quantity})
-        }
-        await fs.writeFile(carritosPath, JSON.stringify(carritos))
-        res.status(200).send("Carrito actualizado correctamente")
-    } else {
-        res.status(404).send({mensaje: "El carrito no existe"})
-    }
-})
+cartRouter.get('/:cid', getCart) //Consultar los productos guardados en un carritp
+cartRouter.post('/', createCart) //Crear un nuevo carrito
+cartRouter.post('/:cid/products/:pid', insertProductCart) //Agregar nuevo producto al carrito
+cartRouter.put('/:cid', updateProductsCart) //Mofidicar totalmente el array de productos del carrito 
+cartRouter.put('/:cid/products/:pid', updateQuantityProductCart)  //Actualizo cantidad de productos
+cartRouter.delete('/:cid', deleteCart)  //Elimino todos los productos del carrito
+cartRouter.delete('/:cid/products/:pid', deleteProductCart) //Elimino producto del carrito
 
 export default cartRouter
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import { create } from 'express-handlebars'
 import { Server } from 'socket.io'
 import path from 'path'
@@ -7,14 +8,30 @@ import productRouter from './routes/productos.routes.js'
 import cartRouter from './routes/carritos.routes.js'
 import multerRouter from './routes/imagenes.routes.js'
 import chatRouter from './routes/chat.routes.js'
+import orderRouter from './routes/orders.routes.js'
 
 const app = express()
-const hbs = create()
+const hbs = create({
+    layoutsDir: path.join(__dirname, 'views', 'layouts'), // Definir el directorio de los layouts
+    defaultLayout: 'main', // El layout predeterminado es main.handlebars
+    extname: '.handlebars', // Definir la extensión de archivo de los templates
+  });
+  
+  app.engine('handlebars', hbs.engine);  // Definir el motor de plantillas
+  app.set('view engine', 'handlebars');  // Definir la extensión por defecto
+  app.set('views', path.join(__dirname, 'views'));  // Establecer el directorio de vistas
+  
 const PORT = 8080
+
 
 const server = app.listen(PORT, () => {
     console.log("Server on port", PORT)
 })
+
+await mongoose.connect("mongodb+srv://darioh364:ZoeTeAmoMucho20@cluster0.aufua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then( () => console.log("Base de Datos conectada"))
+.catch((e) => console.log("Error al conectar con base de datos", e))
+
 
 //Inicializo Socket.io en el servidor
 const io = new Server(server)
@@ -22,21 +39,20 @@ const io = new Server(server)
 app.use(express.json()) //Para manejar JSON en las peticiones
 app.use(express.urlencoded({extended: true}))
 //Configuracion de hbs para localizacion de plantillas y extension de archivo
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
 
-//Establezco el directorio de las vistas
-app.set('views', path.join(__dirname, 'views'))
 
 //Rutas de mi aplicacion
 app.use('/public', express.static(__dirname + '/public'))//Defino la carpeta publica como destino de los archivos estaticos
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 app.use('/api/chat', chatRouter)
+app.use('/api/orders',orderRouter)
 app.use('/upload', multerRouter)
-app.get('/', (req,res) => {
-    res.status(200).send("Ok")
-})
+app.get('/', (req, res) => {
+    res.status(200).render('layouts/main');  // Renderiza 'main.handlebars' desde 'views/layouts'
+});
+
+
 
 let mensajes = []
 //Conexiones de socket.io
